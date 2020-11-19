c838fe8f43009af22188a1b4b010e95bf3055a6d
赵维博 <315596734@qq.com>
2020-11-19 11:11:31 +0800

    master  do nothing

---
+++ app/src/main/java/com/xsb/app/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.xsb.app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
